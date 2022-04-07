6b9089766e27d2903595fbffcb0f11065886ffcb
Marko Vasilic <marko.vasilic1998@gmail.com>
2022-04-07 15:00:45 +0200

    dodan resources

--- ZdravoKorporacija/Repository/PatientRepository.cs
+++ ZdravoKorporacija/Repository/PatientRepository.cs
@@ -1,30 +1,61 @@
+using Model;
 using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Repository
 {
-   public class PatientRepository
-   {
-      private String patientFilePath;
-      
-      public List<Patient> FindAll()
-      {
-         throw new NotImplementedException();
-      }
-      
-      public Model.Patient SavePatient(Model.Patient PacientToMake)
-      {
-         throw new NotImplementedException();
-      }
-      
-      public Boolean RemovePatient(long Jmbg)
-      {
-         throw new NotImplementedException();
-      }
-      
-      public Model.Patient FindOneByJmbg(long Jmbg)
-      {
-         throw new NotImplementedException();
-      }
-   
-   }
+    public class PatientRepository
+    {
+        private String patientFilePath = @"..\..\..\Resources\patients.json";
+
+        public List<Patient> FindAll()
+        {
+            var values = GetValues();
+            return values;
+        }
+
+        public void SavePatient(Patient PacientToMake)
+        {
+            var values = GetValues();
+            values.Add(PacientToMake);
+            Save(values);
+        }
+
+        public void RemovePatient(long Jmbg)
+        {
+            var values = GetValues();
+            values.RemoveAll(value => value.jmbg.Equals(Jmbg));
+            Save(values);
+        }
+
+        public Model.Patient? FindOneByJmbg(long Jmbg)
+        {
+            List<Patient> patients = GetValues();
+            foreach (Patient patient in patients)
+                if (patient.jmbg.Equals(Jmbg))
+                    return patient;
+
+            return null;
+        }
+
+        public void Save(List<Patient> values)
+        {
+            File.WriteAllText(patientFilePath, JsonConvert.SerializeObject(values, Formatting.Indented));
+        }
+
+        public List<Patient> GetValues()
+        {
+            var values = JsonConvert.DeserializeObject<List<Patient>>(File.ReadAllText(patientFilePath));
+
+            if (values == null)
+            {
+                values = new List<Patient>();
+            }
+
+            return values;
+        }
+
+    }
 }
