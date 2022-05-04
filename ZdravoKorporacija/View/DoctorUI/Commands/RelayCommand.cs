93dbade4c5720203d265411c4514596f1fc1b40f
AleksandraPetrovic00 <jovinagimnazija>
2022-05-04 19:16:08 +0200

    doctorWPF mainWindow i homwPage

---
+++ ZdravoKorporacija/View/DoctorUI/Commands/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ZdravoKorporacija.View.DoctorUI.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object>? executeAction;
+        private Predicate<object>? canExecute;
+
+        public RelayCommand(Action<object>? execute)
+        {
+            executeAction = execute;
+        }
+        public RelayCommand(Action<object> execute, Predicate<object> canExec)
+        {
+            executeAction = execute;
+            canExecute = canExec;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (canExecute == null)
+                return true;
+            return canExecute(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public void Execute(object? parameter)
+        {
+            executeAction(parameter);
+        }
+    }
+}
