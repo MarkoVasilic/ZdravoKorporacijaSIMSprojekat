d75292e4fbb9daa93f05a03d22dc359c07d49a3b
Marko Vasilic <marko.vasilic1998@gmail.com>
2022-05-20 14:46:33 +0200

    ispravljen wpf

---
+++ ZdravoKorporacija/View/SecretaryUI/Validation/FilterJmbgValidationRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ZdravoKorporacija.View.SecretaryUI.Validation
+{
+    public class FilterJmbgValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                var text = value as string;
+                Regex r = new Regex("^[0-9]+$");
+                if (!r.IsMatch(text) && text.Length > 0)
+                {
+                    return new ValidationResult(false, "You must enter only digits!");
+                    
+                }
+                if (text.Length > 0 && text.Length != 13)
+                {
+                    return new ValidationResult(false, "Length must be 13!");
+                }
+                return new ValidationResult(true, null);
+            }
+            catch
+            {
+                return new ValidationResult(false, "Unknown error occured.");
+            }
+
+        }
+    }
+}
